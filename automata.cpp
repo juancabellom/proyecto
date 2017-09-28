e499df9ad293a3647bd58b4151eaa644cd3757dd
juancabellom <31752190+juancabellom@users.noreply.github.com>
2017-09-28 07:14:02 -0500

    Add files via upload

---
+++ automata.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+#include <conio.h>
+using namespace std;
+class Nodo
+{
+private:
+Nodo*sig;
+Nodo*inferior;
+Nodo*Primero;
+int estado;
+string caracter;
+string apilo;
+string extraer;
+public:
+friend class MarcAyModelO;
+Nodo(){
+sig=NULL;
+inferior=NULL;
+estado=0;
+caracter="";
+apilo="";
+extraer="";
+Primero=NULL;
+}
+};
